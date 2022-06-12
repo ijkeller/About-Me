e172b36e18f552f98edcd33fadb556277f190dd1
Isaiah Keller <isaiah.keller@gmail.com>
2022-06-12 15:36:33 -0700

    added y/n question to js

--- app.js
+++ app.js
@@ -1,5 +1,51 @@
 'use strict';
-console.log("JavaScript connected")
+
+let user2 = prompt('What is your name?');
+console.log(`user2: ${user2}`)
+alert(`Hello, ${user2}!  You wanted to take a quiz?  Ok...`)
+
+let correct = 0
+
+let movie = prompt('My favorite movie is the Shawshank Redemption? y or n: ');
+console.log(`movie: ${movie}`)
+if (movie.toLowerCase() === 'y') {
+    correct++;
+    console.log(`correct: ${correct}`)
+}
+
+let physiology = prompt('Do I have a degree in Physiology? y or n: ');
+console.log(`physiology: ${physiology}`)
+if (physiology.toLowerCase() === 'n') {
+    correct++;
+    console.log(`correct: ${correct}`)
+}
+
+let toes = prompt('Do I have all ten toes? y or n: ')
+console.log(`toes: ${toes}`)
+if (toes.toLowerCase() === 'y') {
+    correct++;
+    console.log(`correct: ${correct}`)
+}
+
+let sudoku = prompt('Do I like to do Sudoku puzzles? y or n: ')
+console.log(`sudoku: ${sudoku}`)
+if (sudoku.toLowerCase() === 'y') {
+    correct++;
+    console.log(`correct: ${correct}`)
+}
+
+let spain = prompt('Was I born in Spain: ')
+console.log(`sudoku: ${sudoku}`)
+if (sudoku.toLowerCase() === 'n') {
+    correct++;
+    console.log(`correct: ${correct}`)
+}
+
+if (correct >= 3) {
+    alert(`Good job, ${user}!  You got ${correct}/5 correct!`)
+} else {
+    alert(`Nice try, ${user}.  You got ${correct}/5 correct.`)
+}
 
 // let user = prompt('What is your name?');
 // console.log(`user: ${user}`)
@@ -69,43 +115,4 @@ console.log("JavaScript connected")
 
 //  ** Personal Quiz **
 
-let user2 = prompt('What is your name?');
-console.log(`user2: ${user2}`)
-alert(`Hello, ${user2}!  You wanted to take a quiz?  Ok...`)
-
-let correct = 0
-
-let movie = prompt('My favorite movie is the Shawshank Redemption? y or n: ');
-console.log(`movie: ${movie}`)
-if (movie.toLowerCase() === 'y') {
-    correct++;
-    console.log(`correct: ${correct}`)
-}
-
-let physiology = prompt('Do I have a degree in Physiology? y or n: ');
-console.log(`physiology: ${physiology}`)
-if (physiology.toLowerCase() === 'n') {
-    correct++;
-    console.log(`correct: ${correct}`)
-}
-
-let toes = prompt('Do I have all ten toes? y or n: ')
-console.log(`toes: ${toes}`)
-if (toes.toLowerCase() === 'y') {
-    correct++;
-    console.log(`correct: ${correct}`)
-}
-
-let sudoku = prompt('Do I like to do Sudoku puzzles? y or n: ')
-console.log(`sudoku: ${sudoku}`)
-if (sudoku.toLowerCase() === 'y') {
-    correct++;
-    console.log(`correct: ${correct}`)
-}
-
-if (correct >= 3) {
-    alert(`Good job, ${user}!  You got ${correct}/4 correct!`)
-} else {
-    alert(`Nice try, ${user}.  You got ${correct}/4 correct.`)
-}
 
